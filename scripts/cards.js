8d51f7fbc0a5f1c89c42d87a0f3366aa02a3cf09
ansuleymanova <suleymanova.dev@gmail.com>
2022-04-15 00:18:28 +0300

    add Altay for Sasha

--- scripts/cards.js
+++ scripts/cards.js
@@ -21,6 +21,6 @@ const initialCards = [
     },
     {
         name: 'Алтай',
-        link: 'https://downloader.disk.yandex.ru/preview/550976fe8a1d05b73e0c51b784cc33df1785ccb1bf209606fff9846ee64bc749/6258c686/pNFnqCfpuII8MUG2R14v2BzKvGwHOQ9wgZgHy9n-buoROwKF63IRjclaMRSd4GP6VYvK4c7xrmZduk7yg32pSQ%3D%3D?uid=0&filename=altay.jpg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&tknv=v2&size=2880x838'
+        link: '../images/altay.jpg'
     }
 ];
